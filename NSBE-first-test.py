967a7142448e2531f6513e21c6054f54e334c7ce
owen-ste <stephenson1owen@gmail.com>
2021-02-06 15:58:49 -0500

    Update NSBE-first-test.py

--- NSBE-first-test.py
+++ NSBE-first-test.py
@@ -3,24 +3,30 @@ Project Name: NSBE Project
 Authors: Owen Stephenson, Kyle Clahar
 Date: 06.02.2021
 '''
-'''Initial_principal = input("How much are you investing (in dollars): ")
-interest_rate = input("What is the interest rate (in percentage points): ")
-time_period = input("How long will your envestment be (in years): ")
-compounding_period = input("What is the number of times  the interest is applied per time period: ")
-final_amount = input("What is the final amount of money you wish to have (in dollars): ")'''
 
-def intro():
-    print("""
-Please state your yearly income (CAD$)
-For example: 50000
-""")
-    yincome = input()
-    
+def get_suggested_principal():
+    print('Please state your yearly income (CAD$)')
+    yincome = float(input())
+    suggested_principal = float(0)
+    if yincome <= 50000:
+        suggested_principal = yincome * 0.1
+    elif yincome > 50000:
+        suggested_principal = yincome * 0.15
+    return suggested_principal
+
+def interest_rate_calculation(term,principal):
+    interest = 0.5
+    interest += principal * 0.000003
+    interest += term / 12 * 0.4
+    return interest
 
 def main():
     print("*"*20)
     print("Compound Interest Calculator")
-    print("What would you like to calculate?")
-    choice = input("Enter 1 to calculate your final amount \nEnter 2 for to calculate the interest rate needed \nEnter 3 to calculate the investment period needed \nEnter 4 to calculate the initial investment needed \nEnter number here: ")
-    print(choice)
+    print("Your suggested principal investment is",int(get_suggested_principal()),'$')
+    print('How much would you like to invest? (CAD$)')
+    principal = float(input())
+    print('How long would you like to invest your money? (in months)')
+    term = float(input())
+    print('Your Interest Rate is', round(interest_rate_calculation(term,principal),2),'%')
 main()
