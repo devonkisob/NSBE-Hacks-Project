c74623abc0246182200b0e06f4eab3c0166bd53a
owen-ste <stephenson1owen@gmail.com>
2021-02-06 18:45:09 -0500

    Update NSBE-first-test.py

--- NSBE-first-test.py
+++ NSBE-first-test.py
@@ -14,14 +14,14 @@ def get_suggested_principal():
         suggested_principal = yincome * 0.15
     return suggested_principal
 
-def interest_rate_calculation(term,principal):
+def interest_rate_calculation(time,principal):
     interest = 0.5
-    interest += principal * 0.000003
-    interest += term / 12 * 0.4
+    interest += int(principal) * 0.000003
+    interest += time / 12 * 0.4
     return interest
 
     ### finalizing user's principal investment
-def principal_investment_choice():
+def principal_investment_choice(suggested_principal):
     print("Do you know how much you want your principal investment to be?")
     print("State [Y] for yes or [N] for no.")
     know_P = input()
@@ -37,30 +37,33 @@ def principal_investment_choice():
             print("State [A] for suggested principal investment, or [B] for your  own investment value.")
             confirm_suggested_principal = input()
             if confirm_suggested_principal == ('A'):
+                principal = suggested_principal
                 print("Thank you")
                 break
             elif confirm_suggested_principal == ('B'):
                 know_P = ('Y')
                 print("Thank you")
                 continue
+    return principal
 
-def caluculate_final_amount(term,principal,interest):
+def caluculate_final_amount(time,principal,interest):
     final_amount = float(0)
     interest += 1
-    term = term/12
-    interest = pow(interest,term)
+    time = time/12
+    interest = pow(interest,time)
     final_amount = principal * interest
     return final_amount
 
 def main():
     print("*"*20)
     print("Compound Interest Calculator")
-    get_suggested_principal()
-    principal_investment_choice()
+    suggested_principal = get_suggested_principal()
+    
+    principal = principal_investment_choice(suggested_principal)
     print('How long would you like to invest your money? (in months)')
-    term = float(input())
-    interest = float(interest_rate_calculation(term,principal)/100)
-    print('Your Interest Rate is', round(interest_rate_calculation(term,principal),2),'%')
-    final_amount = caluculate_final_amount(term,principal,interest)
-    print('Once your investment reach maturity your final amount will be', round(final_amount,2),'$')
+    time = float(input())
+    interest = float(interest_rate_calculation(time,principal)/100)
+    print('Your Interest Rate is', round(interest_rate_calculation(time,principal),2),'%')
+    final_amount = caluculate_final_amount(time,principal,interest)
+    print('Once your investment reaches maturity your final amount will be', round(final_amount,2),'$')
 main()
