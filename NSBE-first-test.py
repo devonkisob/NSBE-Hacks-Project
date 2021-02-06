ed3cae452b8da654bc44f9573956da232c4ab284
kale <kyleclahar83@gmail.com>
2021-02-06 15:54:21 -0500

    Update NSBE-first-test.py

--- NSBE-first-test.py
+++ NSBE-first-test.py
@@ -1,18 +1,10 @@
-'''
-Project Name: NSBE Project
-Author: Owen Stephenson
-Date: 06.02.2021
-'''
-'''Initial_principal = input("How much are you investing (in dollars): ")
-interest_rate = input("What is the interest rate (in percentage points): ")
-time_period = input("How long will your envestment be (in years): ")
-compounding_period = input("What is the number of times  the interest is applied per time period: ")
-final_amount = input("What is the final amount of money you wish to have (in dollars): ")'''
-
+def interest_rate_calculation(term,principal):
+    interest = 0.5
+    interest += principal * 0.000003
+    interest += term / 12 * 0.4
+    return interest
 def main():
-    print("*"*20)
-    print("Compound Interest Calculator")
-    print("What would you like to calculate?")
-    choice = input("Enter 1 to calculate your final amount \nEnter 2 for to calculate the interest rate needed \nEnter 3 to calculate the investment period needed \nEnter 4 to calculate the initial investment needed \nEnter number here: ")
-    print(choice)
+    principal = float(input('principal: '))
+    term = float(input('term: '))
+    print('Your Interest Rate is', round(interest_rate_calculation(term,principal),2),'%')
 main()
